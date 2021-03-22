2c5ed9df88a43489af20120d0b04c1823595402f
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-03-22 22:11:29 +0900

    Update 선풍기_project.c
    최종_완성

--- 선풍기_project.c
+++ 선풍기_project.c
@@ -7,7 +7,6 @@ int temp = A0; // 온도센서
 int power_toggle = 0; // 상태값
 int speed_toggle = 5; // 속도상태값
 int temp_state =0;
-
 void setup() 
 { 
   pinMode(powerbt, INPUT_PULLUP);
@@ -21,38 +20,44 @@ void setup()
   digitalWrite(4, HIGH);    // Motor B 방향설정1
   digitalWrite(5, LOW);     // Motor B 방향설정2
 } 
- 
+int power_state;
+int sensor;
+float temp_re;
  
 void loop() 
 { 
-  int power_state = digitalRead(powerbt); // 상태값 읽기
-  int sensor = analogRead(temp);
-  float temp_re = (5.0 * sensor *100) / 1024;   // 전압을 온도로 변환
+  power_state = digitalRead(powerbt); // 상태값 읽기
+  sensor = analogRead(temp);
+  temp_re = (5.0 * sensor *100) / 1024;   // 전압을 온도로 변환
   Serial.println(temp_re);
   delay(500);
   if(temp_re >= 33)
   {
-    temp_state = 1;
-    speed_toggle= 4;  
+    analogWrite(3,80);
+    play();
   }
   else temp_state = 0;
-  if(temp_state == 1)
-  {
-    power_toggle =1;
-  }
-  if(power_state == HIGH)
-  {
-    power_toggle = 1;
-  }
-
+  if(temp_state == 1) power_toggle =1;
+  if(power_state == HIGH)power_toggle = 1;
   // 전원버튼 눌렀을때
   if(power_toggle == 1)
   {
-    if(speed_toggle != 1 && speed_toggle != 2 && speed_toggle != 3  && speed_toggle != 0)
+    play();
+  }
+
+  if(power_toggle == 0 && speed_toggle == 0)
+  {
+    analogWrite(3, 0);      // Motor B 속도조절 (0~255)
+    Serial.println("stop");
+    if(power_state == HIGH)
     {
-      analogWrite(motorPin, 80); // 속도 하
-          Serial.println("speed_4");
+      power_toggle = 1;
     }
+  }
+} 
+
+void play()
+{
     int speed_state1 = digitalRead(speed_1); // 상태값 읽기
     int speed_state2 = digitalRead(speed_2); // 상태값 읽기
     int speed_state3 = digitalRead(speed_3); // 상태값 읽기
@@ -81,14 +86,9 @@ void loop()
       speed_toggle = 0;
         power_toggle = 0;
     }
-    if(power_state == HIGH && temp_state ==1)
-    {
-      
-    }
+    if(power_state == HIGH && temp_state ==1){}
 
-  }
-  
-  if(power_toggle == 0 && speed_toggle == 0)
+    if(power_toggle == 0 && speed_toggle == 0)
   {
     analogWrite(3, 0);      // Motor B 속도조절 (0~255)
     Serial.println("stop");
@@ -97,6 +97,5 @@ void loop()
       power_toggle = 1;
     }
   }
-} 
-
+}
 
